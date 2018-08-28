385883ef712b52a83c83f79ea4db504ac95b7599
DESKTOP-PF0UA01\Andy Carolan <andycarolan9@gmail.com>
2018-08-28 20:31:06 +0100

    choose next waypoint created in cpp

---
+++ Source/Testing_Grounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	return EBTNodeResult::Succeeded;
+}
+
+
